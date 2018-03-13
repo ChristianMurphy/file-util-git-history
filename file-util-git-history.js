a872943d13b2dac8ab30d07f9660f52f048fd488
Christian Murphy <cmurphy@unicon.net>
2018-03-13 14:00:11 -0700

    feat: read history of a file using revision walker

--- file-util-git-history.js
+++ file-util-git-history.js
@@ -1,4 +1,5 @@
 const { access } = require("fs");
+const { Repository, Revwalk } = require("nodegit");
 const { isAbsolute, parse, resolve } = require("path");
 const { promisify } = require("util");
 const assert = require("assert");
@@ -26,4 +27,34 @@ async function resolveGitFolder(path) {
   }
 }
 
-module.exports = { resolveGitFolder };
+async function readHistory(filePath, gitPath) {
+  // ensure filepath and gitpath exist
+  await accessAsync(filePath);
+  await accessAsync(gitPath);
+
+  // open repository to HEAD commit
+  const repo = await Repository.open(gitPath);
+  const walker = repo.createRevWalk();
+  walker.pushHead();
+  walker.sorting(Revwalk.SORT.Time);
+
+  // find commits that are related to file
+  // NOTE: this may need to be made recursive to pull extremely long histories
+  const history = await walker.fileHistoryWalk(
+    "file-util-git-history.js",
+    100000
+  );
+
+  // cleanup references to native code
+  walker.free();
+  repo.free();
+
+  return history;
+}
+
+async function gitHistory(filePath, options = {}) {
+  const gitPath = options.gitPath ? gitPath : await resolveGitFolder(filePath);
+  return readHistory(filePath, gitPath);
+}
+
+module.exports = { gitHistory, readHistory, resolveGitFolder };
--- file-util-git-history.spec.js
+++ file-util-git-history.spec.js
@@ -1,16 +1,49 @@
-const { resolveGitFolder } = require("./file-util-git-history");
+const {
+  gitHistory,
+  resolveGitFolder,
+  readHistory
+} = require("./file-util-git-history");
 const { resolve } = require("path");
 
 test("find git folder for current file", async () => {
-  const actual = resolveGitFolder(__filename);
-  const expected = resolve(__dirname, ".git");
-  await expect(actual).resolves.toBe(expected);
+  const actualGitFolder = resolveGitFolder(__filename);
+  const expectedGitFolder = resolve(__dirname, ".git");
+  await expect(actualGitFolder).resolves.toBe(expectedGitFolder);
 });
 
 test("relative path should throw", async () => {
-  await expect(resolveGitFolder(".")).rejects.toThrow();
+  const gitFolder = resolveGitFolder(".");
+  await expect(gitFolder).rejects.toThrow();
 });
 
-test("path with not fit parent should throw", async () => {
-  await expect(resolveGitFolder(resolve(__dirname, ".."))).rejects.toThrow();
+test("path with not git parent should throw", async () => {
+  const gitFolder = resolveGitFolder(resolve(__dirname, ".."));
+  await expect(gitFolder).rejects.toThrow();
+});
+
+test("should read commits", async () => {
+  const history = readHistory(__filename, resolve(__dirname, ".git"));
+  await expect(history).resolves.not.toHaveLength(0);
+});
+
+test("should have first commit", async () => {
+  const history = await readHistory(__filename, resolve(__dirname, ".git"));
+  const firstSha = history[0].commit.sha();
+  // commit where this spec was added
+  await expect(firstSha).toBe("038c2a637df844bdd547e68686a7fc764e6aae31");
+});
+
+test("missing file should fail", async () => {
+  const history = readHistory(__filename + ".dne", resolve(__dirname, ".git"));
+  await expect(history).rejects.toThrow();
+});
+
+test("missing git folder should fail", async () => {
+  const history = readHistory(__filename, resolve(__dirname, ".."));
+  await expect(history).rejects.toThrow();
+});
+
+test("should read commits from abstraction", async () => {
+  const history = gitHistory(__filename);
+  await expect(history).resolves.not.toHaveLength(0);
 });
