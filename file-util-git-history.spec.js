038c2a637df844bdd547e68686a7fc764e6aae31
Christian Murphy <cmurphy@unicon.net>
2018-03-13 12:38:14 -0700

    feat: find parent git folder

--- file-util-git-history.spec.js
+++ file-util-git-history.spec.js
@@ -1,3 +1,17 @@
-test("failing", () => {
-  expect(false).toBe(true);
+const { resolveGitFolder } = require("./file-util-git-history");
+const { resolve } = require("path");
+
+test("find git folder for current file", async () => {
+  expect.assertions(1);
+  const actual = await resolveGitFolder(__filename);
+  const expected = resolve(__dirname, ".git");
+  expect(actual).toBe(expected);
+});
+
+test("relative path should throw", async () => {
+  await expect(resolveGitFolder(".")).rejects.toThrow();
+});
+
+test("path with not fit parent should throw", async () => {
+  await expect(resolveGitFolder(resolve(__dirname, ".."))).rejects.toThrow();
 });
